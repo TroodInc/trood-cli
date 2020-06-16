86275ffff73bd4de7b8292082f8d369b1e5ca91b
Alexander Gor <ornot.work@gmail.com>
2020-06-16 17:09:29 +0300

    print errors if occure

--- trood/cli/spaces.py
+++ trood/cli/spaces.py
@@ -65,6 +65,9 @@ def create(ctx, name: str, template: str):
         if result.status_code == 201:
             data = result.json()
             click.echo(f'Space {data["url"]} created successfully! ')
+        elif result.status_code == 400:
+            data = result.json()
+            click.echo(data['msg'])
     else:
         click.echo(f'Cant create space from [{template}] template')
 
